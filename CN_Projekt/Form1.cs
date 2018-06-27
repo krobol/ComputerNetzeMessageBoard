59c487e0599f8d88f303380371b0585f1ab3075b
Dietrich Trepnau <dietrich.trepnau@st.ovgu.de>
2018-06-27 04:17:26 +0200

    retrieving messages works now

--- CN_Projekt/Form1.cs
+++ CN_Projekt/Form1.cs
@@ -11,19 +11,35 @@ using System.Net;
 
 using System.Net.Http;
 using System.Net.Sockets;
+using Newtonsoft.Json; //you might have to install the pacckage via NuGet
+
 namespace CN_Projekt
 {
+    public class Message
+    {
+        public string id;
+        public string userID;
+        public string text;
+        public string date;
+        public string replyTo;
+        public string replies;
+        public string color;
+        public string reported;
+        public string publicName;
+    }
+
     public partial class Form1 : Form
     {
-        EndPoint epLocal, epRemote;
         static  HttpClient client;
+        private delegate void DELEGATE(); //needed to update ui from another thread
+        static string messages; 
 
         public Form1()
         {
+            messages = "";
             InitializeComponent();
 
             client = new HttpClient();
-
 
             textLocalIP.Text = GetLocalIP();
             textLocalIP.ReadOnly = true;
@@ -31,6 +47,11 @@ namespace CN_Projekt
             textLocalPort.Text = "80";
             textFriendsPort.Text = "8080";
 
+            //refresh messages every second
+            System.Timers.Timer timer = new System.Timers.Timer();
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(refreshMessageBoard);
+            timer.Interval = 1000;      
+            timer.Enabled = true;
         }
 
         private string GetLocalIP()
@@ -51,24 +72,40 @@ namespace CN_Projekt
 
         }
 
-        private void ButtonSend_Click(object sender, EventArgs e)
-        {
-            Task<string> task = saveMessageAsync(textMessage.Text);
-            System.Diagnostics.Debug.WriteLine(task.Result);
-        }
-
-
-        private async Task<string> saveMessageAsync(string message)
+        private async void ButtonSend_Click(object sender, EventArgs e)
         {
             var values = new Dictionary<string, string>
             {
-                { "text", message },
+                { "text", textMessage.Text },
             };
             var content = new FormUrlEncodedContent(values);
             var response = await client.PostAsync("http://" + textFriendsIP.Text + "/comment/saveComment", content);
 
             var responseString = await response.Content.ReadAsStringAsync();
-            return responseString;
+        }
+
+        private void refreshMessageBoard(object sender, EventArgs e)
+        {
+            //create delegate to update ui element, otherwise an exception is thrown since this is not the thread that created the ui element
+            Delegate del = new DELEGATE(_refreshMessageBoard);
+            this.Invoke(del);
+        }
+
+        private async void _refreshMessageBoard()
+        {
+            var responseString = await client.GetStringAsync("http://" + textFriendsIP.Text + "/messageBoard/getMessages");
+
+            if (responseString == messages)
+                return;
+
+            messages = responseString;
+            var list = JsonConvert.DeserializeObject<List<Message>>(responseString);
+            listMessage.Items.Clear();
+            foreach (var message in list)
+            {
+               listMessage.Items.Add("Friend: " + message.text);
+            }
+
         }
     }
 }
